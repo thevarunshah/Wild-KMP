f31be904765a8f76b86fbccb983e7a88c37915ec
Varun Shah <thevarunshah@gmail.com>
2016-05-16 23:59:44 -0400

    initial commit

---
+++ src/KMP.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class KMP {
+
+	public static int search(String text, String pattern){
+		
+		int t = text.length();
+		int p = pattern.length();
+		//if pattern length is longer than text length
+		if(p > t){
+			return -1;
+		}
+		
+		//create dfa
+		DFA prefixTable = new DFA(pattern);
+		
+		int matchLength = 0;
+		for(int i = 0; i < t; i++){
+			
+			boolean reset = false;
+			//back-track on failure
+			while(matchLength > 0 && pattern.charAt(matchLength) != text.charAt(i)){
+				//check if fail was due to wildcard
+				if(pattern.charAt(matchLength) == '*'){
+					//loop-back with KMP - double check already matched pattern
+					if(matchLength > 0 && reset){
+						int kmpValue = KMP.search(text.substring(i-matchLength, i), pattern.substring(0, matchLength));
+						if(kmpValue != 0){
+							//reset match
+							matchLength = 0;
+						}
+					}
+					break;
+				}
+				
+				matchLength = prefixTable.table[matchLength-1]; //fall-back
+				reset = true;
+			}
+			
+			//match or wildcard
+			if(pattern.charAt(matchLength) == text.charAt(i) || pattern.charAt(matchLength) == '*'){
+				matchLength++; //matched
+			}
+			
+			//found the pattern
+			if(matchLength == p){
+				return i-(p-1);
+			}
+		}
+		
+		//couldn't find the pattern
+		return -1;
+	}
+	
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the text: ");
+		String text = sc.nextLine();
+		System.out.println("Enter the pattern: ");
+		String pattern = sc.nextLine();
+		sc.close();
+		
+		int index = KMP.search(text, pattern);
+		if(index != -1){
+			System.out.println(index);
+		}
+		else{
+			System.out.println("Couldn't find pattern in the text.");
+		}
+	}
+}
